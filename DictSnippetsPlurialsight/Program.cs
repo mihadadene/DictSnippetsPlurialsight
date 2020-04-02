276800018383ba083e20e541e4b4090e2ba9f926
mihadadene <mi.ihadadene@gmail.com>
2020-04-01 21:45:51 -0400

    first commit

---
+++ DictSnippetsPlurialsight/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictSnippetsPlurialsight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Country norway = new Country("Norway", "NOR", "Europe", 5_282_223);
+            Country finland = new Country("Finland", "FIN", "Europe",5_511_303);
+
+           var countries = new Dictionary<string, Country>()
+
+           {
+               { norway.Code, norway },
+               { finland.Code, finland }
+           };
+
+            Country selectedCountry = countries["NOR"];
+            Console.WriteLine(selectedCountry.Name);
+
+            foreach(Country country in countries.Values)
+            Console.WriteLine(country.Name);
+            Console.ReadKey();
+
+        }
+    }
+}
